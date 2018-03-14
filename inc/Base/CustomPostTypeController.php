d8ed4aab0f6e390bf50fd381216b7b93975eac1d
MaxDrljic <max_spree@hotmail.com>
2018-03-14 13:25:26 +0100

    Added full functionality for CPT

--- inc/Base/CustomPostTypeController.php
+++ inc/Base/CustomPostTypeController.php
@@ -17,12 +17,11 @@ class CustomPostTypeController extends BaseController
 
   public $subpages = array();
 
+  public $custom_post_types = array();
+
   public function register()
   {
-    $option = get_option( 'max_plugin' );
-    $activated = isset($option['cpt_manager']) ? $option['cpt_manager'] : false;
-
-    if ( ! $activated) return;
+    if ( ! $this->activated( 'cpt_manager' ) ) return;
 
     $this->settings = new SettingsApi();
 
@@ -32,7 +31,11 @@ class CustomPostTypeController extends BaseController
 
     $this->settings->addSubPages( $this->subpages )->register();
 
-    add_action( 'init', array( $this, 'activate' ) );
+    $this->storeCustomPostTypes();
+
+    if ( ! empty( $this->custom_post_types ) ) {
+    add_action( 'init', array( $this, 'registerCustomPostTypes' ) );
+    }
   }
 
   public function setSubpages()
@@ -49,17 +52,38 @@ class CustomPostTypeController extends BaseController
     );
   }
 
-  public function activate()
+  public function storeCustomPostTypes()
   {
-    register_post_type( 'max_products',
+    $this->custom_post_types = array(
       array(
-        'labels' => array(
-          'name' => 'Products',
-          'singular_name' => 'Product'
-        ),
+        'post_type' => 'max_product',
+        'name' => 'Products',
+        'singular_name' => 'Product',
         'public' => true,
-        'has_archive' => true,
+        'has_archive' => true
+      ), array(
+        'post_type' => 'max_comics',
+        'name' => 'Comic Books',
+        'singular_name' => 'Comic Book',
+        'public' => true,
+        'has_archive' => false
       )
     );
+  }
+
+  public function registerCustomPostTypes()
+  {
+    foreach ($this->custom_post_types as $post_type) {
+      register_post_type( $post_type['post_type'],
+        array(
+          'labels' => array(
+            'name' => $post_type['name'],
+            'singular_name' => $post_type['singular_name']
+          ),
+          'public' => $post_type['public'],
+          'has_archive' => $post_type['has_archive']
+        )
+      );
+    } 
   }
 }
---
+++ inc/Base/TestimonialController.php
@@ -0,0 +1,47 @@
+<?php
+/**
+  * @package MaxPlugin
+  */
+namespace Inc\Base;
+
+use \Inc\Api\SettingsApi;
+use \Inc\Base\BaseController;
+use \Inc\Api\Callbacks\AdminCallbacks;
+
+/**
+ * 
+ */
+class TestimonialController extends BaseController
+{
+  public $callbacks;
+
+  public $subpages = array();
+
+  public function register()
+  {
+    if ( ! $this->activated( 'testimonial_manager' ) ) return;
+
+    $this->settings = new SettingsApi();
+
+    $this->callbacks = new AdminCallbacks();
+
+    $this->setSubpages();
+
+    $this->settings->addSubPages( $this->subpages )->register();
+
+  }
+
+  public function setSubpages()
+  {
+    $this->subpages = array(
+      array(
+        'parent_slug' => 'max_plugin',
+        'page_title' => 'Testimonial Manager',
+        'menu_title' => 'Testimonial Manager',
+        'capability' => 'manage_options',
+        'menu_slug' => 'max_testimonial',
+        'callback' => array( $this->callbacks, 'adminTestimonial' )
+      )
+    );
+  }
+}
