d7178898d72f4e2ee959d7303a93705f09dc9e00
MaxDrljic <max_spree@hotmail.com>
2018-03-14 08:44:22 +0100

    Properly saving default options on activation in the settings manager

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -9,5 +9,13 @@ class Activate
 {
   public static function activate() {
     flush_rewrite_rules();
+
+    if ( get_option( 'max_plugin' ) ) {
+      return;
+    }
+
+    $default = array();
+
+    update_option( 'max_plugin', $default );
   }
 }  
