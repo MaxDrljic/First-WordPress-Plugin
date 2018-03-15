81dedf6208b482b4e08b1519a625d147050a269d
MaxDrljic <max_spree@hotmail.com>
2018-03-15 09:54:33 +0100

    Added one-way full functionality for CPT, everything works correctly

--- inc/Api/Callbacks/AdminCallbacks.php
+++ inc/Api/Callbacks/AdminCallbacks.php
@@ -13,25 +13,9 @@ class AdminCallbacks extends BaseController
     return require_once( "$this->plugin_path/templates/admin.php" );
   }
 
-  /* public function maxOptionsGroup( $input )
+  public function adminCpt()
   {
-    return $input;
+    return require_once( "$this->plugin_path/templates/cpt.php" );
   }
 
-  public function maxAdminSection()
-  {
-    echo 'Check this beautiful section!';
-  } */
-
-  public function maxTextExample()
-  {
-    $value = esc_attr( get_option( 'text_example' ) );
-    echo '<input type="text" class="regular-text" name="text_example" value="' . $value . '" placeholder="Write Something Here!" />';
-  }
-
-  public function maxFirstName()
-  {
-    $value = esc_attr( get_option( 'first_name' ) );
-    echo '<input type="text" class="regular-text" name="text_example" value="' . $value . '" placeholder="Write Your First Name!" />';
-  }
 }
--- templates/admin.php
+++ templates/admin.php
@@ -12,11 +12,11 @@
     <div id="tab-1" class="tab-pane active">
 
       <form method="post" action="options.php">
-      <?php
-        settings_fields( 'max_plugin_settings' );
-        do_settings_sections( 'max_plugin' );
-        submit_button();
-      ?>
+        <?php
+          settings_fields( 'max_plugin_settings' );
+          do_settings_sections( 'max_plugin' );
+          submit_button();
+        ?>
       </form>
 
     </div>
