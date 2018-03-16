0eda489cc2e213e733eb1d8b2f10494c73257160
MaxDrljic <max_spree@hotmail.com>
2018-03-16 09:08:55 +0100

    Added CPT table to show all registered Custom Post Types & updated/cleaned the code

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -10,12 +10,14 @@ class Activate
   public static function activate() {
     flush_rewrite_rules();
 
-    if ( get_option( 'max_plugin' ) ) {
-      return;
-    }
-
     $default = array();
 
-    update_option( 'max_plugin', $default );
+    if ( ! get_option( 'max_plugin' ) ) {
+      update_option( 'max_plugin', $default );
+    }
+
+    if ( ! get_option( 'max_plugin_cpt' ) ) {
+      update_option( 'max_plugin_cpt', $default );
+    }
   }
 }  
--- src/js/myscript.js
+++ src/js/myscript.js
@@ -7,19 +7,18 @@ window.addEventListener("load", () => {
     tabs[i].addEventListener("click", switchTab);
   }
 
-  function switchTab(e) {
-    e.preventDefault();
+  function switchTab(event) {
+    event.preventDefault();
 
     document.querySelector("ul.nav-tabs li.active").classList.remove("active");
     document.querySelector(".tab-pane.active").classList.remove("active");
 
-    let clickedTab = e.currentTarget;
-    let anchor = e.target;
+    let clickedTab = event.currentTarget;
+    let anchor = event.target;
     let activePaneID = anchor.getAttribute("href");
 
     clickedTab.classList.add("active");
     document.querySelector(activePaneID).classList.add("active");
     
   }
-
 });
