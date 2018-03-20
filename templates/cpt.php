2a62445242bfe4b96c8a2805abc8da6c418cb79d
MaxDrljic <max_spree@hotmail.com>
2018-03-20 08:52:52 +0100

    Added Edit section for CPT

--- templates/cpt.php
+++ templates/cpt.php
@@ -4,13 +4,19 @@
 
 
   <ul class="nav nav-tabs">
-    <li class="active"><a href="#tab-1">Your Custom Post Types</a></li>
-    <li><a href="#tab-2">Add Custom Post Type</a></li>
+    <li class="<?php echo !isset($_POST["edit_post"]) ? 'active' : '' ?>">
+      <a href="#tab-1">
+        Your Custom Post Types
+      </a></li>
+    <li class="<?php echo isset($_POST["edit_post"]) ? 'active' : '' ?>">
+      <a href="#tab-2">
+      <?php echo isset($_POST["edit_post"]) ? 'Edit' : 'Add' ?>
+      </a></li>
     <li><a href="#tab-3">Export</a></li>
   </ul>
 
   <div class="tab-content">
-    <div id="tab-1" class="tab-pane active">
+    <div id="tab-1" class="tab-pane <?php echo !isset($_POST["edit_post"]) ? 'active' : '' ?>">
 
       <h3>Manage your Custom Post Types</h3>
 
@@ -25,7 +31,12 @@
           $public = isset($option['public']) ? "TRUE" : "FALSE";
           $archive = isset($option['has_archive']) ? "TRUE" : "FALSE";
 
-          echo "<tr><td>{$option['post_type']}</td><td>{$option['singular_name']}</td><td>{$option['plural_name']}</td><td class=\"text-center\">{$public}</td><td class=\"text-center\">{$archive}</td><td class=\"text-center\"><a href=\"#\">EDIT</a>";
+          echo "<tr><td>{$option['post_type']}</td><td>{$option['singular_name']}</td><td>{$option['plural_name']}</td><td class=\"text-center\">{$public}</td><td class=\"text-center\">{$archive}</td><td class=\"text-center\">";
+
+          echo '<form method="post" action="" class="inline-block">';
+          echo '<input type="hidden" name="edit_post" value="'. $option['post_type'] .'">';
+          submit_button( 'Edit', 'primary small', 'submit', false );
+          echo '</form> ';
 
           echo '<form method="post" action="options.php" class="inline-block">';
           settings_fields( 'max_plugin_cpt_settings' );
@@ -42,7 +53,7 @@
   
     </div>
 
-    <div id="tab-2" class="tab-pane">
+    <div id="tab-2" class="tab-pane <?php echo isset($_POST["edit_post"]) ? 'active' : '' ?>">
       <form method="post" action="options.php">
           <?php
             settings_fields( 'max_plugin_cpt_settings' );
