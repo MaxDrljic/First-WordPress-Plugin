a5077be717b1d34056926e9a78e290d3ae10cccd
MaxDrljic <max_spree@hotmail.com>
2018-05-23 08:27:53 +0200

    Created Testimonial UI and functionality.

--- inc/Base/TestimonialController.php
+++ inc/Base/TestimonialController.php
@@ -51,40 +51,67 @@ class TestimonialController extends BaseController
   {
     add_meta_box(
       'testimonial_author',
-      'Author',
-      array( $this, 'render_author_box' ),
+      'Testimonial Options',
+      array( $this, 'render_features_box' ),
       'testimonial',
       'side',
       'default'
     );
-
-    // Metabox 2: author email
-
-    // Metabox 3: approved[checkbox]
-
-    // Metabox 4: featured[checkbox]
   }
 
-  public function render_author_box($post)
+  public function render_features_box($post)
   {
-    wp_nonce_field( 'max_testimonial_author', 'max_testimonial_author_nonce' );
+    wp_nonce_field( 'max_testimonial', 'max_testimonial_nonce' );
 
-    $value = get_post_meta( $post->ID, '_max_testimonial_author_key', true );
-
+    $data = get_post_meta( $post->ID, '_max_testimonial_key', true );
+    $name = isset($data['name']) ? $data['name'] : '';
+    $email = isset($data['email']) ? $data['email'] : '';
+    $approved = isset($data['approved']) ? $data['approved'] : false;
+    $featured = isset($data['featured']) ? $data['featured'] : false;
     ?>
-    <label for="max_testimonial_author">Testimonial Author</label>
-    <input type="text" id="max_testimonial_author" name="max_testimonial_author" value="<?php echo esc_attr( $value ) ?>">
+    <p>
+      <label class="meta-label" for="max_testimonial_author">Author Name</label>
+      <input type="text" id="max_testimonial_author"
+        name="max_testimonial_author" class="widefat" value="<?php echo esc_attr( $name ); ?>">
+    </p>
+
+    <p>
+      <label for="max_testimonial_email" class="meta-label">Author Email</label>
+      <input type="email" id="max_testimonial_email" name="max_testimonial_email"
+      class="widefat" value="<?php echo esc_attr( $email ); ?>">
+    </p>
+
+    <div class="meta-container">
+      <label for="max_testimonial_approved" class="meta-label w-50 text-left">Approved</label>
+      <div class="text-right w-50 inline">
+      <br />
+        <div class="ui-toggle inline"><input type="checkbox" id="max_testimonial_approved" name="max_testimonial_approved" value="1" <?php echo $approved ? 'checked' : ''; ?>>
+          <label for="max_testimonial_approved"><div></div></label>
+        </div>
+      </div>
+    </div>
+    <br />
+    <div class="meta-container">
+      <label for="max_testimonial_featured" class="meta-label w-50 text-left">Featured</label>
+      <div class="text-right w-50 inline">
+      <br />
+        <div class="ui-toggle inline"><input type="checkbox" id="max_testimonial_featured" name="max_testimonial_featured" value="1" <?php echo $featured ? 'checked' : ''; ?>>
+          <label for="max_testimonial_featured"><div></div></label>
+        </div>
+      </div>
+    </div>
+
     <?php
   }
 
   public function save_meta_box($post_id)
   {
-    if (! isset($_POST['max_testimonial_author_nonce'])) {
+    if (! isset($_POST['max_testimonial_nonce'])) {
       return $post_id;
     }
 
-    $nonce = $_POST['max_testimonial_author_nonce'];
-    if (! wp_verify_nonce( $nonce, 'max_testimonial_author' )) {
+    $nonce = $_POST['max_testimonial_nonce'];
+    if (! wp_verify_nonce( $nonce, 'max_testimonial' )) {
       return $post_id;
     }
 
@@ -96,7 +123,12 @@ class TestimonialController extends BaseController
       return $post_id;
     }
 
-    $data = sanitize_text_field( $_POST['max_testimonial_author'] );
-    update_post_meta( $post_id, '_max_testimonial_author_key', $data );
+    $data = array(
+      'name' => sanitize_text_field( $_POST['max_testimonial_author'] ),
+      'email' => sanitize_text_field( $_POST['max_testimonial_email'] ),
+      'approved' => isset($_POST['max_testimonial_approved']) ? 1 : 0,
+      'featured' => isset($_POST['max_testimonial_featured']) ? 1 : 0,
+    );
+    update_post_meta( $post_id, '_max_testimonial_key', $data );
   }
 }
