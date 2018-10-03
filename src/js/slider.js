c47d5d104e9d4d6f4de60926bdbc71506d78b3dd
MaxDrljic <max_spree@hotmail.com>
2018-10-03 13:03:41 +0200

    Slider completed

--- src/js/slider.js
+++ src/js/slider.js
@@ -7,13 +7,38 @@ const sliderView = document.querySelector('.max-slider--view > ul'),
 
 
 // Sliding function
+const slideMe = (sliderViewItems, isActiveItem) => {
+  // Update the classes
+  isActiveItem.classList.remove('is-active');
+  sliderViewItems.classList.add('is-active');
 
+
+  // Css transform the active slide position
+  sliderView.setAttribute('style', 'transform:translateX(-' + sliderViewItems.offsetLeft + 'px)');
+}
 
 
 
 // Before sliding function
-const beforeSliding = (i) => {
-  console.log(i);
+// No parentheses for parameters because there is only one parameter
+const beforeSliding = i => {
+  let isActiveItem = document.querySelector('.max-slider--view__slides.is-active'),
+    currentItem = Array.from(sliderViewSlides).indexOf(isActiveItem) + i,
+    nextItem = currentItem + i,
+    sliderViewItems = document.querySelector(`.max-slider--view__slides:nth-child(${nextItem})`);
+
+  // if nextItem is bigger than the number of slides
+  if (nextItem > sliderLength) {
+    sliderViewItems = document.querySelector('.max-slider--view__slides:nth-child(1)');
+  }
+
+  // if nextItem is 0
+  if (nextItem == 0) {
+    sliderViewItems = document.querySelector(`.max-slider--view__slides:nth-child(${sliderLength})`);
+  }
+
+  // Trigger the sliding method
+  slideMe(sliderViewItems, isActiveItem);
 }
 
 
