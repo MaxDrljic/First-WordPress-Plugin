2aede2db8b511dded711c8ce64d00b49a15e3786
MaxDrljic <max_spree@hotmail.com>
2018-10-03 12:44:10 +0200

    slider.js global variables & testing function that will trigger when an arrow is clicked

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,3 +1,22 @@
-document.addEventListener('DOMContentLoaded', function(e) {
-  //
-});
+// Global(not really global, only for this file) variables
+const sliderView = document.querySelector('.max-slider--view > ul'),
+  sliderViewSlides = document.querySelectorAll('.max-slider--view__slides'),
+  arrowLeft = document.querySelector('.max-slider--arrows__left'),
+  arrowRight = document.querySelector('.max-slider--arrows__right'),
+  sliderLength = sliderViewSlides.length;
+
+
+// Sliding function
+
+
+
+
+// Before sliding function
+const beforeSliding = (i) => {
+  console.log(i);
+}
+
+
+// Triggers arrows
+arrowRight.addEventListener('click', () => beforeSliding(1));
+arrowLeft.addEventListener('click', () => beforeSliding(0));
